f0374c1e582b78fe6c8b4da5ecf306ceb371bcf5
Jakub Bas <71661812+jakubbas@users.noreply.github.com>
2023-03-20 18:51:27 +0000

    Init Unreal

---
+++ Source/NeuralNetworkUnreal/NeuralNetworkUnrealGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "NeuralNetworkUnrealGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ANeuralNetworkUnrealGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ANeuralNetworkUnrealGameMode();
+};
+
+
+
